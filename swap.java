cf59dce33cef1e184373cbb6e88626a0693e01eb
SriHariniganji <srinijakobbai@gmail.com>
2024-07-13 10:46:42 +0530

    first commit

---
+++ swap.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class swap
+{
+	public static void main(String args[])
+	{
+		int a=10;
+		int b=20;
+		int c;
+		System.out.println("The numbers before swapping a = "+a+" b = "+b);
+		c=a;
+		a=b;
+		b=c;
+		System.out.println("The numbers after swapping a = "+a+" b = "+b);
+	}
+}
